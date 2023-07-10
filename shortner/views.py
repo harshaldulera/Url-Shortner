ee3d3194f494aaa62cf892ac9ad22944ad552456
harshaldulera <harshaldulera82@gmail.com>
2023-07-10 23:37:02 +0530

    link

--- shortner/views.py
+++ shortner/views.py
@@ -11,6 +11,6 @@ def create(request):
     if request.method == 'POST':
         link = request.POST['link']
         uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=link, uuid=uid)
+        new_url = Url(link=link,uuid=uid)
         new_url.save()
         return HttpResponse(uid)
