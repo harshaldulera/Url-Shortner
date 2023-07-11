b97791d173aab20ed092db72f28ee385da810ae0
harshaldulera <harshaldulera82@gmail.com>
2023-07-11 09:01:08 +0530

    db traceback

--- shortner/models.py
+++ shortner/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Url(models.Model):
-    link = models.CharField(max_length=10000),
+    link = models.CharField(max_length=10000)
     uuid = models.CharField(max_length=10)
