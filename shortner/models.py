208b59a0b26a2db5af6cc565bf221ba2e37e6f57
harshaldulera <harshaldulera82@gmail.com>
2023-07-11 09:15:28 +0530

    final html & uuid tweak

--- shortner/models.py
+++ shortner/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Url(models.Model):
-    link = models.CharField(max_length=10000)
+    link = models.CharField(max_length=10000, default='')
     uuid = models.CharField(max_length=10)
